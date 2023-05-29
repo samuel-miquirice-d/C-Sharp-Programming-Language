4fde9fdcaced9bb7a0e53862930817248c2fb660
samuel-miquirice <samiquid@gmail.com>
2023-05-29 19:34:30 -0300

    Mundando pela introdução do comentário.

--- test.cs
+++ test.cs
@@ -4,13 +4,13 @@ var stock = new Stock("MSFT");
 stock.PriceChanged += ReportPriceChange;    // Define a função que será executada quando o evento ocorrer
 stock.Price = 123;                          // Price changed from 0 to 123
 stock.Price = 456;                          // Price changed from 123 to 456  
-
+// Alteração realizada por Samuel António Miquirice Domingos (com outro nome de usuário)
 void ReportPriceChange(decimal oldPrice, decimal newPrice)
 {
   Console.WriteLine("Price changed from " + oldPrice + " to " + newPrice);
 }
 
-// Alteração realizada por Samuel António Miquirice Domingos
+
 
 public delegate void PriceChangedHandler(decimal oldPrice, decimal newPrice);
 
@@ -18,7 +18,7 @@ public class Stock
 {
   string symbol;
   decimal price;
-  // Outra alteração realizada por Samuel António Miquirice Domingos
+  
   public Stock(string symbol) { this.symbol = symbol; }
   
   public event PriceChangedHandler PriceChanged;        // Variável de evento
