c7f1d63b93f0667f678da1e38d822ffe7916bca7
SamuelMiquirice <samuel.miquirice@gmail.com>
2023-05-29 14:49:54 -0300

    teste

---
+++ test.cs
@@ -0,0 +1,38 @@
+
+namespace teste;
+var stock = new Stock("MSFT");
+stock.PriceChanged += ReportPriceChange;    // Define a função que será executada quando o evento ocorrer
+stock.Price = 123;                          // Price changed from 0 to 123
+stock.Price = 456;                          // Price changed from 123 to 456  
+
+void ReportPriceChange(decimal oldPrice, decimal newPrice)
+{
+  Console.WriteLine("Price changed from " + oldPrice + " to " + newPrice);
+}
+
+// Alteração realizada por Samuel António Miquirice Domingos
+
+public delegate void PriceChangedHandler(decimal oldPrice, decimal newPrice);
+
+public class Stock
+{
+  string symbol;
+  decimal price;
+  // Outra alteração realizada por Samuel António Miquirice Domingos
+  public Stock(string symbol) { this.symbol = symbol; }
+  
+  public event PriceChangedHandler PriceChanged;        // Variável de evento
+  
+  public decimal Price
+  {
+    get { return price; }
+    set
+    {
+      if (price == value) return;      
+      decimal oldPrice = price;
+      price = value;
+      if (PriceChanged != null)         
+        PriceChanged(oldPrice, price);   // Dispara o evento
+    }
+  }
+}
