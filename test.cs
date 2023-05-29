e5227e8203450494043e7adf71d6b0a66786d1f5
samuel-miquirice <samiquid@gmail.com>
2023-05-29 20:01:32 -0300

    Mundando pela mudança de posição do primeiro comentário deste usuário.

--- test.cs
+++ test.cs
@@ -1,10 +1,10 @@
-
+// Alteração realizada por Samuel António Miquirice Domingos (com outro nome de usuário)
 namespace teste;
 var stock = new Stock("MSFT");
 stock.PriceChanged += ReportPriceChange;    // Define a função que será executada quando o evento ocorrer
 stock.Price = 123;                          // Price changed from 0 to 123
 stock.Price = 456;                          // Price changed from 123 to 456  
-// Alteração realizada por Samuel António Miquirice Domingos (com outro nome de usuário)
+
 void ReportPriceChange(decimal oldPrice, decimal newPrice)
 {
   Console.WriteLine("Price changed from " + oldPrice + " to " + newPrice);
